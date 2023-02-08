cee6ddb467fe316a9cb1deaaecd8164d9241836b
JIBIN SM <jibijibin232@gmail.com>
2023-02-09 00:19:56 +0530

    greatest integer

---
+++ greatestnumber.py
@@ -0,0 +1,10 @@
+a=int(input("first number"))
+b=int(input("second number"))
+c=int(input("third number"))
+if(a>=b and a>=c):
+    print('first number is great   ', a)
+if(b>=a and b>=c):
+    print('second number is great   ' , b)
+if(c>=b and c>=a):
+    print('third number is great  ', c)
+
